2edfe8bf5a16d55dc9866cd05ef5530869e0ac5e
Michael Mount <mount.photos@gmail.com>
2023-11-12 14:53:31 -0700

    Added twitch image and link, added rel=noreferrer to allow for links to open in new tab and adjusted margins to account for new button in box

--- links/src/App.js
+++ links/src/App.js
@@ -1,20 +1,53 @@
 import "./App.css";
 
-import tiktok from "./images/tiktok.png"
-import discord from "./images/discord.png"
-import insta from "./images/Insta.png"
+import tiktok from "./images/tiktok.png";
+import discord from "./images/discord.png";
+import insta from "./images/Insta.png";
+import twitch from "./images/unnamed-1.png";
 
 function App() {
   return (
     <div className="flex justify-center items-center h-screen">
-      
-      <div className="socialBox flex-col items-center justify-center md:mb-12 mb-36" >
-      <h1 className="font-custom text-6xl my-3 text-green">Soical Links</h1>
-       <ul className="w-72 h-92 rounded-lg bg-green">
-        <li><a href="https://www.tiktok.com/@mmount.tv"><img src={tiktok} alt="Tiktok Link" className="p-5"/></a></li>
-        <li><a href="https://discord.gg/yY4xXdbF"><img src={discord} alt="Discord Link" className="p-5"/></a></li>
-        <li><a href="https://www.instagram.com/michael_mount_/"><img src={insta} alt="Tiktok Link" className="p-5"/></a></li>
-       </ul>
+      <div className="socialBox flex-col items-center justify-center md:mt-64 mt-7">
+        <h1 className="font-custom text-6xl my-3 text-green">Soical Links</h1>
+        <ul className="w-72 h-92 rounded-lg bg-green">
+          <li>
+            <a
+              href="https://www.twitch.tv/mmount"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <img src={twitch} alt="Twitch Link" className="p-5" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.tiktok.com/@mmount.tv"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <img src={tiktok} alt="Tiktok Link" className="p-5" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://discord.gg/yY4xXdbF"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <img src={discord} alt="Discord Link" className="p-5" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/michael_mount_/"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <img src={insta} alt="Insta Link" className="p-5" />
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
   );
